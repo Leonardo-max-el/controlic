25c10be5d04ff5946137d922cb27cdaf726f87d5
Leonardo Kevin Quito Espiritu <lquito@continental.edu.pe>
2025-02-08 14:25:56 -0500

    Mi_primer_avance

---
+++ app_control/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render (request, 'signup.html', {'form': UserCreationForm()})
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'Usuario creado correctamente'})
+            except:
+                return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'El usuario ya existe'})
+        else:
+            return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'Las contraseñas no coinciden'})
